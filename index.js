53098ba84d9899f85af40be79ad8a09b2e21c58c
root <root@tony.>
2025-05-10 17:46:14 -0400

    Your commit message

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
+  let isLocked = false;
+
+  //Nested arrow function to handle login
+  const loginAttempt = (username, password) => {
+    if (isLocked) {
+      console.log("Account is locked.");
+      return;
+    }
+
+    attemptCount++; //attempt count
+
+    if (username === userInfo.username && password === userInfo.password) {
+      console.log(`Welcome, ${userInfo.username}!`);
+      attemptCount = 0; // Reset
+    } else {
+      const remaining = maxAttempts - attemptCount;
+      console.log(`Incorrect. Attempts left: ${remaining}`);
+
+      if (attemptCount >= maxAttempts) {
+        isLocked = true;
+        console.log("Account locked due to too many failed attempts.");
+      }
+    }
+  };
+
+  return loginAttempt;
+}
 
 
 
