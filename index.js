e349b042adb3443e038a5ef27325977732176110
MiuTonny <miltonp.tavares@hotmail.com>
2025-05-10 18:21:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ function createLoginTracker(userInfo) {
   const maxAttempts = 3;
   let isLocked = false;
 
-  return function loginAttempt(username, password) {
+  return function loginAttempt(passwordAttempt) {
     if (isLocked) {
       return "Account locked due to too many failed login attempts";
     }
 
-    if (username === userInfo.username && password === userInfo.password) {
-      attemptCount = 0; // reset attempts 
+    if (passwordAttempt === userInfo.password) {
+      attemptCount = 0;
       return "Login successful";
-    } else {
-      attemptCount++;
-      if (attemptCount >= maxAttempts) {
-        isLocked = true;
-        return "Account locked due to too many failed login attempts";
-      } else {
-        return `Attempt ${attemptCount}: Login failed`;
-      }
     }
+
+    attemptCount++;
+
+    //3rd failure to return as a failed attempt
+    const response = `Attempt ${attemptCount}: Login failed`;
+
+    if (attemptCount >= maxAttempts) {
+      isLocked = true;
+    }
+
+    return response;
   };
 }
 
